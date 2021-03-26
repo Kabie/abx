07e04d02917d5d4bac6c90151c5b4b7158aa83e3
Kabie <kabie+git@outlook.com>
2021-03-26 18:07:48 +0800

    search address

--- lib/ether.ex
+++ lib/ether.ex
@@ -48,4 +48,49 @@ defmodule Ether do
   def hexdigit("0x" <> hex) do
     String.to_integer(hex, 16)
   end
+
+  def make_key() do
+    :crypto.strong_rand_bytes(32)
+    |> make_key()
+  end
+
+  def make_key(privkey) do
+    {:ok, pkey} = ABX.Types.Hash.cast(privkey)
+    {address_of(pkey.bytes), Base.encode16(pkey.bytes, case: :lower)}
+  end
+
+  def search_address(prefix, suffix, tries \\ 10000) do
+    _search_address(String.downcase(prefix), String.downcase(suffix), tries)
+  end
+
+  defp _search_address(_prefix, _suffix, 0), do: nil
+
+  defp _search_address(prefix, suffix, tries) do
+    privkey = :crypto.strong_rand_bytes(32)
+    "0x" <> address = address_of(privkey)
+
+    if String.starts_with?(address, prefix) and
+         String.ends_with?(address, suffix) do
+      {address_of(privkey), Base.encode16(privkey)}
+    else
+      _search_address(prefix, suffix, tries - 1)
+    end
+  end
+
+  def para_search_address(prefix, suffix, n \\ 1, concurrency \\ 4, tries \\ 10_000) do
+    1..concurrency
+    |> Task.async_stream(
+      fn _ ->
+        search_address(prefix, suffix, tries)
+      end,
+      max_concurrency: concurrency,
+      ordered: false,
+      timeout: :infinity
+    )
+    |> Stream.reject(fn {_, result} -> is_nil(result) end)
+    |> Stream.map(fn {_, {addr, priv}} ->
+      {addr, priv}
+    end)
+    |> Enum.take(n)
+  end
 end
