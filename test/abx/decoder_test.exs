8450e28153b5c146f90dc6eb16e81f8f00ccb9c3
Kabie <kabie+git@outlook.com>
2022-04-22 11:39:49 +0800

    fix decode_type call

--- test/abx/decoder_test.exs
+++ test/abx/decoder_test.exs
@@ -6,14 +6,14 @@ defmodule ABX.DecoderTest do
   alias ABX.Types.Address
 
   test "decode_type" do
-    assert Decoder.decode_type(<<1::256>>, :address, <<1::256>>) == Address.cast(1)
+    assert Decoder.decode_type(<<1::256>>, :address, 0) |> elem(1) == Address.cast(1) |> elem(1)
   end
 
   test "decode int<X>" do
     int128_1 = <<0::signed-128, 1::signed-128>>
-    assert Decoder.decode_type(int128_1, {:int, 128}, int128_1) == {:ok, 1}
+    assert Decoder.decode_type(int128_1, {:int, 128}, 0) == {:ok, 1, 32}
 
     int128_neg1 = << -1::signed-128, -1::signed-128>>
-    assert Decoder.decode_type(int128_neg1, {:int, 128}, int128_neg1) == {:ok, -1}
+    assert Decoder.decode_type(int128_neg1, {:int, 128}, 0) == {:ok, -1, 32}
   end
 end
