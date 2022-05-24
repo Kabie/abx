f64f73f974da8f63ddf9b5c91bbb3497f5ae73fc
Kabie <kabie+git@outlook.com>
2022-05-24 19:06:50 +0800

    add wallet sign & recover

--- lib/abx/helpers.ex
+++ lib/abx/helpers.ex
@@ -30,8 +30,8 @@ defmodule ABX.Helpers do
         }
   @type signed_raw_txn :: <<_::16, _::_*8>>
 
-  @spec sign_transaction(txn_obj(), Ether.privkey()) :: signed_raw_txn()
-  def sign_transaction(transaction, private_key) do
+  @spec do_sign_transaction(txn_obj(), Ether.privkey()) :: signed_raw_txn()
+  def do_sign_transaction(transaction, private_key) do
     case transaction do
       # Post EIP-1559, with format EIP-2718
       # TransactionType 2
@@ -39,7 +39,8 @@ defmodule ABX.Helpers do
       # we use empty access_list here for simplicity
       %{chain_id: chain_id, nonce: nonce, priority_fee: priority_fee, gas_price: gas_price, gas_limit: gas_limit, to: to, value: value, data: data} ->
         msg_to_sign = <<0x02>> <> ExRLP.encode([chain_id, nonce, priority_fee, gas_price, gas_limit, to, value, data, []])
-        {r, s, y_parity} = make_signature(msg_to_sign, private_key)
+        {r, s, v} = make_signature(msg_to_sign, private_key)
+        y_parity = v - 27
 
         <<0x02>> <> ExRLP.encode([chain_id, nonce, priority_fee, gas_price, gas_limit, to, value, data, [], y_parity, r, s])
 
@@ -47,14 +48,16 @@ defmodule ABX.Helpers do
       # rlp([nonce, gasPrice, gasLimit, to, value, data, v, r, s])
       %{chain_id: chain_id, nonce: nonce, gas_price: gas_price, gas_limit: gas_limit, to: to, value: value, data: data} ->
         msg_to_sign = ExRLP.encode([nonce, gas_price, gas_limit, to, value, data, chain_id, 0, 0])
-        {r, s, y_parity} = make_signature(msg_to_sign, private_key)
+        {r, s, v} = make_signature(msg_to_sign, private_key)
+        y_parity = v - 27
         v = chain_id * 2 + 35 + y_parity
         ExRLP.encode([nonce, gas_price, gas_limit, to, value, data, v, r, s])
 
       # LegacyTransaction, pre EIP-155
       %{nonce: nonce, gas_price: gas_price, gas_limit: gas_limit, to: to, value: value, data: data} ->
         msg_to_sign = ExRLP.encode([nonce, gas_price, gas_limit, to, value, data])
-        {r, s, y_parity} = make_signature(msg_to_sign, private_key)
+        {r, s, v} = make_signature(msg_to_sign, private_key)
+        y_parity = v - 27
         v = y_parity + 27
         ExRLP.encode([nonce, gas_price, gas_limit, to, value, data, v, r, s])
     end
@@ -71,7 +74,7 @@ defmodule ABX.Helpers do
       |> Map.update(:data, <<>>, &normalize_data/1)
 
     signed_txn =
-      sign_transaction(tco, private_key)
+      do_sign_transaction(tco, private_key)
       |> Base.encode16()
 
     "0x" <> signed_txn
@@ -98,6 +101,41 @@ defmodule ABX.Helpers do
       |> Ether.keccak_256()
       |> Curvy.sign(private_key, compact: true, compressed: false, hash: false)
 
-    {r, s, v - 27}
+    {r, s, v}
+  end
+
+  def wallet_message(message) do
+    "\x19Ethereum Signed Message:\n#{byte_size(message)}#{message}"
+  end
+
+  def wallet_sign(message, private_key) do
+    {r, s, v} =
+      wallet_message(message)
+      |> make_signature(private_key)
+
+    Base.encode16(<<r::256, s::256, v>>)
+  end
+
+  def wallet_recover_address(message, signature) do
+    addr =
+      wallet_message(message)
+      |> recover_pubkey(signature)
+      |> Ether.keccak_256()
+      |> binary_part(12, 20)
+      |> Base.encode16(case: :lower)
+
+    "0x" <> addr
+  end
+
+  def recover_pubkey(message, signature) do
+    {r, s, v} = unpack_signature(signature)
+    Curvy.recover_key(<<v, r::256, s::256>>, message, hash: false)
+    |> Curvy.Key.to_pubkey()
+    |> binary_part(1, 64)
+  end
+
+  defp unpack_signature("0x" <> signature) do
+    {:ok, <<r::256, s::256, v>>} = Base.decode16(signature, case: :mixed)
+    {r, s, v}
   end
 end
